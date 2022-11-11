98772e010ed125168bfed19dae218a0c25bd4a97
Liam Burnand <liamb1216@gmail.com>
2022-11-11 03:37:57 +0000

    Added middleware for checking origins of requests and making sure that any change is a real request

---
+++ middleware/middleware.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	echoMw "github.com/labstack/echo/v4/middleware"
+)
+
+// New initialises web server middleware
+func New(e *echo.Echo, domainName string) {
+	config := echoMw.CORSConfig{
+		AllowCredentials: true,
+		Skipper:          echoMw.DefaultSkipper,
+		AllowOrigins: []string{
+			"http://afc." + domainName,
+			"https://afc." + domainName,
+			"http://afcaldermaston.co.uk",
+			"https://afcaldermaston.co.uk",
+			"http://" + domainName,
+			"https://" + domainName,
+		},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowCredentials, echo.HeaderAccessControlAllowOrigin, echo.HeaderAuthorization},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}
+
+	e.Pre(echoMw.RemoveTrailingSlash())
+	e.Use(echoMw.Logger())
+	e.Use(echoMw.Recover())
+	e.Use(echoMw.CORSWithConfig(config))
+}
